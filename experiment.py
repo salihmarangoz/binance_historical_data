575d8720a0bd64ee423a10ce1c87ea2b4c17862b
NatRetsel <natretseltries@gmail.com>
2025-05-06 20:46:14 +0800

    hotfix supplementing missing monthly data for daily data

--- experiment.py
+++ experiment.py
@@ -4,4 +4,4 @@ import os
 
 if __name__ == "__main__":
     data_dumper = BinanceDataDumper(os.getcwd(),"spot","klines","1d")
-    data_dumper.dump_data(["BTCUSDT"], date_start=None, date_end=None, is_to_update_existing=False, int_max_tickers_to_get=None,tickers_to_exclude=["UST"])
+    data_dumper.dump_data(["BTCUSDT"], date_start=None, date_end="2025-03-30", is_to_update_existing=False, int_max_tickers_to_get=None,tickers_to_exclude=["UST"])
